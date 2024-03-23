ec5167c93818f7f1792a4c2f6a2ca1780267153e
David Mikeladze <davidmikeladze00@gmail.com>
2024-03-23 13:25:08 +0400

    feat: add header

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,9 @@
-<script setup></script>
+<script setup>
+import Header from "@/components/Header.vue";
+</script>
 
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="./assets/logo.svg"
-      width="125"
-      height="125"
-    />
-
-    <div>
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+  <Header />
 
   <main>
     <TheWelcome />
---
+++ src/components/Header.vue
@@ -0,0 +1,31 @@
+<script setup>
+import cardShopImage from "@/assets/icon-card.png";
+import cartShopImage from "@/assets/icon-cart.png";
+</script>
+
+<template>
+  <header
+    class="border-[1px] px-[20px] py-[10px] flex justify-between items-center"
+  >
+    <div class="flex gap-[10px] items-center">
+      <img :src="cardShopImage" class="w-[50px] h-[50px]" alt="" />
+      <h3 class="text-[white]">Card Shop</h3>
+    </div>
+    <div class="flex gap-[10px] items-center">
+      <img
+        :src="cartShopImage"
+        class="w-[50px] h-[50px] cursor-pointer"
+        alt=""
+      />
+      <button
+        class="border-[1px] px-[10px] py-[5px] text-[white] duration-500 rounded-[5px] hover:bg-[white] hover:text-[#04011d]"
+      >
+        GALLERY
+      </button>
+    </div>
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
